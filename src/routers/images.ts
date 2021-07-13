1c459fe4d1713dd0177c48989ea62c1db78bd9c2
Anastasia <anastaisafedotova@gmail.com>
2021-07-13 11:35:40 +0200

    feat: add get images method

--- src/routers/images.ts
+++ src/routers/images.ts
@@ -1,10 +1,35 @@
 import { Router } from "express";
+import fs from 'fs';
 import upload from './../storage/storagesetting';
+
 const imagesApi = Router();
 
 imagesApi.post("/", upload, (req , res) => {
   console.log(req.file);
   res.status(200).send({uploaded: true});
+});
+
+imagesApi.get("/:imgname", upload, (req , res) => {
+  const imgname = req.params.imgname;
+
+  fs.readFile(`./dist/uploads/${imgname}`, (err, image) => {
+    if (err) {
+      res.statusCode = 404;
+      res.json('not found');
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "image/jpeg");
+      res.end(image);
+    }
+  });
+   /*const readableStream = fs.createReadStream(`./dist/uploads/${imgname}`);
+  res.setHeader("Content-Type", "image/jpeg");
+  readableStream.on("data", function(){
+    readableStream.pipe(res);
+  });
+
+
+  res.end();*/
 });
 
 export default imagesApi;
