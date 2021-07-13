a64b0e597149ccf05d235d60d6b7457dc4130716
Anastasia <anastaisafedotova@gmail.com>
2021-07-13 13:20:02 +0200

    refactor: add image-proxy router

--- src/routers/image-proxy.ts
+++ src/routers/image-proxy.ts
@@ -1,9 +1,26 @@
 import { Router } from "express";
+import fs from "fs";
+import sharp from "sharp";
 
 const imageProxyApi = Router();
 
-imageProxyApi.get("/", async (_req, _res) => {
-  console.log('image proxy')
+imageProxyApi.get("/images/:imgname/params", async (req, res) => {
+  const { width, height, _format } = req.query;
+  const imgname = req.params.imgname;
+
+
+  fs.readFile(`./dist/uploads/${imgname}`, async (err, image) => {
+    if (err) {
+      res.statusCode = 404;
+      res.json('not found');
+    } else {
+      await sharp(image).extract({ width: +width, height: +height, left: 0, top: 0 }).toBuffer().then(data => {
+        res.end(data)
+      })
+    }
+  });
+
+
 });
 
 export default imageProxyApi;
